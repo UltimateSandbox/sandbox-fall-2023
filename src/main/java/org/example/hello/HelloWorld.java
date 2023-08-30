a66ca414ca824b924363b96ab739c729f68170cf
spyro1001 <raymond.hedgecock@gmail.com>
2023-08-29 18:50:14 -0700

    adding hello world

---
+++ src/main/java/org/example/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.example.hello;
+
+public class HelloWorld {
+
+    // shortcut -> psvm
+    public static void main(String[] args) {
+        // shortcut -> sout
+        System.out.println("Hello World!");
+    }
+
+}
